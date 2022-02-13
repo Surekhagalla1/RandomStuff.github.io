528ba33871d1a82a42782b9c88a1a210f1a71199
GS <71748824+Surekhagalla1@users.noreply.github.com>
2022-02-13 11:58:13 -0600

    Create app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+function s(){
+
+        var name = getElementById("name");
+        var place = getElementById("place");
+        var metal = getElementById("metal");
+        var color = getElementById("color");
+        var para = getElementById("p").innerHtml;
+        var t = "Once upon a time, "+name+" was walking through the "+place+". "+name+" found a "+color+" "+metal+"."
+        var stor = getElementById("p")
+        stor.innerHtml=t    
+    }
