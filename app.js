991239d24661be11d006229dbe9f0f826db37d03
GS <71748824+Surekhagalla1@users.noreply.github.com>
2022-02-13 12:13:04 -0600

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ function s(){
         var metal = getElementById("metal");
         var color = getElementById("color");
         var para = getElementById("p").innerHtml;
-        var t = "Once upon a time, "+name+" was walking through the "+place+". "+name+" found a "+color+" "+metal+"."
         var stor = getElementById("p")
-        stor.innerHtml=t    
+        stor.innerHtml=
+        "Once upon a time, "+name+" was walking through the "+place+". "+name+" found a "+color+" "+metal+"."
     }
